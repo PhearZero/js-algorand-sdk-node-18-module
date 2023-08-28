f6f5d775f8662a2a32787aaa3a11fe5f4f1c8d8b
Michael J Feher <github@phearzero.com>
2023-08-27 20:05:47 -0500

    add working example

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-import {Indexer} from 'algosdk'
+import algosdk, {Indexer} from 'algosdk'
+
+const indexer = new Indexer(
+    "",
+    "https://mainnet-idx.algonode.cloud/",
+    443
+)
+
+const res = await indexer.lookupAssetByID(312769).do()
+console.log(algosdk.Indexer === Indexer)
+console.log(res)
