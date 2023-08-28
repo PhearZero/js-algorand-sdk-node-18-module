cc7043443e3f254862f590febb219c7b2c0c2e66
Michael J Feher <github@phearzero.com>
2023-08-27 20:17:22 -0500

    fix: install working repo cjs example

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-import algosdk, {Indexer} from 'algosdk'
-
+const algosdk= require('algosdk');
+const {Indexer} = algosdk;
 const indexer = new Indexer(
     "",
     "https://mainnet-idx.algonode.cloud/",
     443
 )
 
-const res = await indexer.lookupAssetByID(312769).do()
-console.log(algosdk.Indexer === Indexer)
-console.log(res)
+indexer.lookupAssetByID(312769).do().then(res=>{
+    console.log(algosdk.Indexer === Indexer)
+    console.log(res)
+
+})
